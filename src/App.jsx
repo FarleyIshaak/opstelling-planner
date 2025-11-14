670261dec51958a45e9da7fa99dbdf38692a56e2
FarleyIshaak <62348641+FarleyIshaak@users.noreply.github.com>
2025-11-14 16:25:49 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import OpstellingPlanner from "./OpstellingPlanner";
+
+export default function App() {
+  return (
+    <div>
+      <OpstellingPlanner />
+    </div>
+  );
+}
