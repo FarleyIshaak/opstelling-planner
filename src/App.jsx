b38011e458cc0e2774b1278965539af04b6106b1
FarleyIshaak <62348641+FarleyIshaak@users.noreply.github.com>
2025-11-14 16:05:33 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import OpstellingPlanner from "./OpstellingPlanner";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 p-4">
+      <OpstellingPlanner />
+    </div>
+  );
+}
